240981dc7d83e192f114e32ed5820e8d3f5562d8
Logon Aluno <brunobiletsky@gmail.com>
2024-04-25 09:22:38 -0300

    Adicionando css e script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Declarando as variaveis
+
+let p1=prompt("Digite seu NOME");
+let p2=prompt("Digite seu PODER");
+let p3=prompt("Digite o nome de um VILÃO");
+let p4=prompt("Digtie um LUGAR");
+let msg= document.getElementById("msg");
+
+msg.innerHTML =`Olá ${p1}, o seu poder é ${p2} e você vai enfrentar o(a) ${p3} no(a)/em ${p4}` /*Escrevendo na pagina */
